0fab3e360def192320e455269edf5fe97bd9b282
Lee Fowler <fowler.lee8@gmail.com>
2020-10-11 15:32:18 +0200

    Documentation for the Logger interface was done

---
+++ Documentation/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.lee","c":"Logger","l":"Logger()","url":"%3Cinit%3E()"},{"p":"com.lee","c":"Logger","l":"logToDataBase()"},{"p":"com.lee","c":"Logger","l":"userInput(List<? extends Transactions>)","url":"userInput(java.util.List)"}]
