a688065422ef6a9c2ee7d9a1465682f501b8461f
Lee Fowler <fowler.lee8@gmail.com>
2020-10-14 02:37:39 +0200

    The main.java file has been updated to perform the console operations for the application.

--- src/com/lee/Main.java
+++ src/com/lee/Main.java
@@ -1,15 +1,13 @@
 package com.lee;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) {
 
-
-
+        /*
 
         //make List of type Transactions and read into appropriate files
         FileHandler fileLogger = new FileHandler();
@@ -31,34 +29,141 @@ public class Main {
 //        dataHandler.search(new Transactions(true,"12/12/2020", 1000.5));
 //        dataHandler.search("files");
 //        System.out.println("check = " + check);
-//        dataHandler.readTransactions();
+        dataHandler.readTransactions();
 
-//        System.out.println("Welcome to the BankApp \n");
-//        System.out.println("Please choose an option below: \n");
-//        System.out.println("(1) Show items \n");
-//        System.out.println("(2) Add items \n");
-//        System.out.println("(3) Edit items \n");
-//        System.out.println("(4) Save and Exit. \n");
+            List<String> list = dataHandler.readToList("Resources/AllTransactions.txt");
+          	Iterator<String> itr = list.iterator();
+            System.out.println();
 
-//        Scanner scanner = new Scanner((System.in));
+          	while (itr.hasNext()) {
+                System.out.println(itr.next());
+            }
 
-//        int iterations = 0;
-//        while(scanner.nextInt() != 4 && iterations < 2){
-//
-//            iterations++;
-//        }
+          	*/
 
 
+            boolean ON = true;
+            int counts = 0;
 
-//        TreeMap<Animal, AnimalLogger> treeMap = new TreeMap<>();
-//
-//        Animal john = new Animal(1,
-//                "john",
-//                2,
-//                "beef");
-//        treeMap.put(john,johnLogger);
+        do {
+
+            System.out.println("Welcome to the Banking Application \n");
+            System.out.println("Please choose an option below: \n");
+            System.out.println("(1) Show items \n");
+            System.out.println("(2) Add items \n");
+            System.out.println("(3) Edit items \n");
+            System.out.println("(4) Save and Exit. \n");
+
+            Scanner scanner = new Scanner((System.in));
+            int option;
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("You chose to show the stored items. Please select from the given options. ");
+                    System.out.println("(1) Show all items \n");
+                    System.out.println("(2) Show expense(s) items \n");
+                    System.out.println("(3) Show income(s) items \n");
+
+                    int showOption = scanner.nextInt();
+
+                        switch (showOption) {
+                            case 1:
+                                System.out.println("You chose to show all items.");
+
+                                break;
+                            case 2:
+                                System.out.println("You chose to show expense(s) only.");
+
+                                break;
+                            case 3:
+                                System.out.println("You chose to show income(s) only.");
+
+                                break;
+                            default:
+                                System.err.println("Please enter a valid option from the menu. You will be re-directed to the main menu.");
+                                break;
+                        }
+
+                    break;
+                case 2:
+                    System.out.println("You chose to add items. Please select from the given options. ");
+
+                        System.out.println("(1) Add expense(s) \n");
+                        System.out.println("(2) Add income(s) \n");
+
+                        int addOption = scanner.nextInt();
+
+                        switch (addOption) {
+                            case 1:
+                                System.out.println("You chose to add expense(s).");
+                                break;
+                            case 2:
+                                System.out.println("You chose to add income(s).");
+                                break;
+                            default:
+                                System.err.println("Please enter a valid option from the menu. You will be re-directed to the main menu.");
+                                break;
+                        }
+
+                    break;
+
+                case 3:
+                    System.out.println("You chose to edit items. Please select from the given options. ");
+
+                    System.out.println("(1) Edit item \n");
+                    System.out.println("(2) Remove item \n");
+
+                    int editOption = scanner.nextInt();
+
+                    switch (editOption) {
+                        case 1:
+                            System.out.println("You chose to edit an item.");
+                            break;
+                        case 2:
+                            System.out.println("You chose to remove an item.");
+                            break;
+                        default:
+                            System.err.println("Please enter a valid option from the menu. You will be re-directed to the main menu.");
+                            break;
+                    }
+
+
+                    break;
+                case 4:
+                    System.out.println("You chose to save and exit. Goodbye.");
+                    //userInput -> save items
+                    //fileInput -> read to expenses and incomes
+                    ON = false;
+                    break;
+                default:
+                    System.err.println("Please enter a valid option from the menu.");
+
+            }
 
 
 
-    }
-}
+        } while(ON && counts<5);
+
+
+
+
+
+
+
+
+    } // end main()
+} // end class Main
+
+
+
+
+
+
+
+
+
+
+
+
+
